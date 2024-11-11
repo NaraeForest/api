fbcf81e3c3935473af157a66c48506e66b156e0f
narumir <narumir@narumir.io>
2024-11-11 14:12:48 +0000

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,29 @@ import {
   NestFactory,
 } from '@nestjs/core';
 import {
+  ConfigService,
+} from '@nestjs/config';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import {
   AppModule,
 } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  if (!configService.get<boolean>("isProduction")) {
+    const config = new DocumentBuilder()
+      .setTitle("05-project")
+      .setDescription("05-project API Document")
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, documentFactory);
+  }
+  const port = configService.get<number>("port");
+  await app.listen(port);
 }
 
 bootstrap();
