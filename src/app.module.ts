e47013d8104cd2fe9279eadbef18514b053afb75
narumir <narumir@narumir.io>
2024-11-11 13:37:09 +0000

    create readonly and writable datasources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import Configuration from './config/configuration';
 import {
   Module,
 } from '@nestjs/common';
+import {
+  ReadOnlyDataSource,
+  WritableDataSource,
+} from './config/database';
 
 @Module({
   imports: [
     Configuration,
+    WritableDataSource,
+    ReadOnlyDataSource,
   ],
   controllers: [],
   providers: [],
---
+++ src/config/database.ts
@@ -0,0 +1,53 @@
+import {
+  ConfigModule,
+  ConfigService,
+} from "@nestjs/config";
+import {
+  TypeOrmModule,
+  TypeOrmModuleOptions,
+} from "@nestjs/typeorm";
+
+const defaultDataSoruceOption: TypeOrmModuleOptions = {
+  type: "postgres",
+  database: "05-project",
+  schema: "public",
+  entities: [
+
+  ],
+};
+
+export const ReadOnlyDataSource = TypeOrmModule.forRootAsync({
+  name: "readonly",
+  imports: [
+    ConfigModule,
+  ],
+  inject: [
+    ConfigService,
+  ],
+  useFactory: (configService: ConfigService) => ({
+    ...defaultDataSoruceOption,
+    host: configService.getOrThrow("database.readonly.host"),
+    port: configService.getOrThrow("database.readonly.port"),
+    username: configService.getOrThrow("database.readonly.username"),
+    password: configService.getOrThrow("database.readonly.password"),
+    synchronize: false,
+  }),
+});
+
+export const WritableDataSource = TypeOrmModule.forRootAsync({
+  name: "writable",
+  imports: [
+    ConfigModule,
+  ],
+  inject: [
+    ConfigService,
+  ],
+  useFactory: (configService: ConfigService) => ({
+    ...defaultDataSoruceOption,
+    host: configService.getOrThrow("database.writable.host"),
+    port: configService.getOrThrow("database.writable.port"),
+    username: configService.getOrThrow("database.writable.username"),
+    password: configService.getOrThrow("database.writable.password"),
+    synchronize: !configService.getOrThrow("isProduction"),
+  }),
+});
