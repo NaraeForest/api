58eb486c62d3fcf43ea9f04af12fc02eed39e67b
narumir <narumir@narumir.io>
2024-11-13 01:41:03 +0000

    change to HttpStatus and fix typo

--- src/social/naver/naver-login.controller.ts
+++ src/social/naver/naver-login.controller.ts
@@ -31,11 +31,11 @@ export class NaverLoginController {
     @Res() res: Response,
   ) {
     if (req.csrfToken() !== state) {
-      return res.status(403).json({ error: "Invalid csrf token" });
+      return res.status(HttpStatus.FORBIDDEN).json({ error: "Invalid csrf token" });
     }
     if (error != null) {
       console.error(error, errorDescription);
-      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: "Internal server Error" });
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: "Internal server error" });
     }
     const token = await this.loginService.requestAccessToken(code, state);
     const profile = await this.loginService.requestProfile(token.accessToken);
