8c391e93357a673308e7f337d41a26ce142229b7
narumir <narumir@narumir.io>
2024-11-15 04:58:34 +0000

    add loki transport

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
+import winston from 'winston';
+import LokiTransport from 'winston-loki';
 import {
   NestFactory,
 } from '@nestjs/core';
@@ -12,10 +14,30 @@ import {
 import {
   AppModule,
 } from './app.module';
+import {
+  WinstonModule,
+} from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const instance = winston.createLogger({
+    format: winston.format.json(),
+    transports: [
+      new LokiTransport({
+        host: configService.getOrThrow("loki.host"),
+        labels: {
+          app: "05-project-server",
+          pod: configService.get("podname"),
+        },
+        json: true,
+      })
+    ],
+  });
+  const logger = WinstonModule.createLogger({
+    instance,
+  });
+  app.useLogger(logger);
   if (!configService.get<boolean>("isProduction")) {
     const config = new DocumentBuilder()
       .setTitle("05-project")
@@ -44,7 +66,7 @@ async function bootstrap() {
    */
   app.enableShutdownHooks();
   app
-    .use(cookieParser(configService.getOrThrow("cookieSecret")))
+    .use(cookieParser(configService.getOrThrow("cookieSecret")));
   const port = configService.get<number>("port");
   await app.listen(port);
 }
